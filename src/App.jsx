0bcd4a5d8bf652ac832a1ac2baac97e5496c7e5b
Malhar <ml575444@dal.ca>
2025-05-14 23:05:40 -0300

    Added features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 const App = () => {
   
@@ -12,8 +13,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
-
-     
+      <Features />
       
     </main>
   )
