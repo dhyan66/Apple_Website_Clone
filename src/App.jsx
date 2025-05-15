11a9146b17d501912bbf78df1d01460d07d07cac
Malhar <ml575444@dal.ca>
2025-05-14 22:10:43 -0300

    Added Animations to util

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import VideoCarousel from "./components/VideoCarousel";
+import Model from "./components/Model";
 
 const App = () => {
   
@@ -11,6 +12,8 @@ const App = () => {
      <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+
      
       
     </main>
